1d22ebfab5447e1b34adc91f134ebbd7bc245839
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-07-15 02:33:45 -0300

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Course.destroy_all
+Student.destroy_all
+
+10.times do
+  Course.create!(
+    name: Faker::Superhero.power,
+    status: Status.list.sample,
+    description: Faker::Space.agency
+  )
+  Student.create!(
+    name: Faker::Superhero.name,
+    status: Status.list.sample,
+    register_number: Faker::StarWars.droid
+  )
+end
