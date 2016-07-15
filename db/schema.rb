52f06400e10ba272290de11ec83f91a7f008b53d
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-07-15 02:03:48 -0300

    Create classroom

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715030809) do
+ActiveRecord::Schema.define(version: 20160715043142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20160715030809) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "classrooms", force: true do |t|
+    t.integer  "course_id"
+    t.integer  "student_id"
+    t.date     "entry_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "classrooms", ["course_id"], name: "index_classrooms_on_course_id", using: :btree
+  add_index "classrooms", ["student_id"], name: "index_classrooms_on_student_id", using: :btree
 
   create_table "courses", force: true do |t|
     t.string   "name",        limit: 45
