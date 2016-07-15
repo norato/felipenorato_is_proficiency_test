e4c6828548e1a07891012468561943808ac8f46d
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-07-14 21:29:38 -0300

    Config locales

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module FelipenoratoIsProficiencyTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Brasilia'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.available_locales = [:en, :"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
+    config.encoding = "utf-8"
   end
 end
