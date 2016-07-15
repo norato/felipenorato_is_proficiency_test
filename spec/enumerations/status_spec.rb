15da2bb6eacfe61df3047261ba7a9b84d73a6a95
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-07-15 01:29:17 -0300

    fix test error

--- spec/enumerations/status_spec.rb
+++ spec/enumerations/status_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Status, type: :model  do
   context 'Validations' do
     it { Status.list.to_a.should eq([1,0]) }
-    it { Status.to_a.should eq([['Active', 1], ['Inactive', 0]]) }
+    it { Status.to_a.should eq([['Ativo', 1], ['Inativo', 0]]) }
   end
 end
